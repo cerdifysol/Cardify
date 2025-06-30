a860fa3039b438de59d379c104377b6855d9e4a3
cerdifysol <cerdify.sol@gmail.com>
2025-06-30 20:21:23 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,33 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
-import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
-import Home    from './pages/Home';
-import Issue   from './pages/Issue';
-import Verify  from './pages/Verify';
-import Header from './components/Header';
-import Profile from './pages/Profile';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import Home from "./pages/Home";
+import Issue from "./pages/Issue";
+import Verify from "./pages/Verify";
+import Header from "./components/Header";
+import Profile from "./pages/Profile";
+import Footer from "./components/Footer";
 
-const NETWORK = 'https://api.devnet.solana.com';
+const NETWORK = "https://api.devnet.solana.com";
 const wallets = [new PhantomWalletAdapter()];
 
 const App = () => (
   <ConnectionProvider endpoint={NETWORK}>
     <WalletProvider wallets={wallets} autoConnect>
       <WalletModalProvider>
-       <Header />
-          <Routes>
-            <Route path="/"        element={<Home   />} />
-            <Route path="/issue"   element={<Issue  />} />
-            <Route path="/verify"  element={<Verify />} />
-            <Route path="/profile"  element={<Profile />} />
-          </Routes>
-   
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/issue" element={<Issue />} />
+          <Route path="/verify" element={<Verify />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+        <Footer />
       </WalletModalProvider>
     </WalletProvider>
   </ConnectionProvider>
